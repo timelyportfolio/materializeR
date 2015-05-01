8cd08a03a565c48a2c993f35e19dfaf3d8386092
timelyportfolio <kent.russell@timelyportfolio.com>
2015-05-01 12:42:41 -0500

    add sidebar example

--- inst/examples/materialize_htmltools.R
+++ inst/examples/materialize_htmltools.R
@@ -9,9 +9,21 @@ library(pipeR)
   <a href="#!" class="collection-item">Alan</li>
   <a href="#!" class="collection-item">Alan<span class="badge">14</span></a>
 </ul>
+
+<nav>
+  <ul class="right hide-on-med-and-down">
+    <li><a href="#!">First Sidebar Link</a></li>
+    <li><a href="#!">Second Sidebar Link</a></li>
+  </ul>
+  <ul id="slide-out" class="side-nav">
+    <li><a href="#!">First Sidebar Link</a></li>
+    <li><a href="#!">Second Sidebar Link</a></li>
+  </ul>
+  <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+</nav>
 ' %>>%
   HTML %>>%
   tagList(
-    materialize()
+    materialize( tasks = list( htmlwidgets::JS('function(){$(".button-collapse").sideNav()}')  ) )
   ) %>>%
   browsable()
