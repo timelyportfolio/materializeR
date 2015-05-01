cac9793130350a8970b96063da5f82cf6cb7ec27
timelyportfolio <kent.russell@timelyportfolio.com>
2015-05-01 14:23:31 -0500

    move nav to top and use show-on-large

--- inst/examples/materialize_htmltools.R
+++ inst/examples/materialize_htmltools.R
@@ -3,24 +3,20 @@ library(htmltools)
 library(pipeR)
 
 '
+<nav>
+  <ul id="slide-out" class="side-nav">
+    <li><a href="#!">First Sidebar Link</a></li>
+    <li><a href="#!">Second Sidebar Link</a></li>
+  </ul>
+  <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="mdi-navigation-menu"></i></a>
+</nav>
+
 <ul class="collection">
   <a href="#!" class="collection-item">Alan<span class="badge">1</span></a>
   <a href="#!" class="collection-item">Alan<span class="new badge">4</span></a>
   <a href="#!" class="collection-item">Alan</li>
   <a href="#!" class="collection-item">Alan<span class="badge">14</span></a>
 </ul>
-
-<nav>
-  <ul class="right hide-on-med-and-down">
-    <li><a href="#!">First Sidebar Link</a></li>
-    <li><a href="#!">Second Sidebar Link</a></li>
-  </ul>
-  <ul id="slide-out" class="side-nav">
-    <li><a href="#!">First Sidebar Link</a></li>
-    <li><a href="#!">Second Sidebar Link</a></li>
-  </ul>
-  <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
-</nav>
 ' %>>%
   HTML %>>%
   tagList(
