084c6b473a237f55ec3b0fa7733ecb8780618983
timelyportfolio <kent.russell@timelyportfolio.com>
2015-05-01 16:35:44 -0500

    fill out DESCRIPTION and barely document

--- R/materialize.R
+++ R/materialize.R
@@ -1,6 +1,14 @@
 #' Add \href{http://materializecss.com/}{materialize} to your R
 #'
-#' <Add Description>
+#' One and only function offered by \code{materializeR}.  The \code{...} is offered but really not all that helpful
+#' currently except for one secret power.  Use of the package \code{htmltools}
+#' from RStudio is a highly encouraged ally for \code{materializeR}.
+#'
+#' @param ... expansion room.  The \code{...} currently are a placeholder for future functionality.
+#' @param width \code{integer} width in pixel for container.  \code{width = 0} since the expectation is
+#'          for the container to be an invisible byproduct.
+#' @param height \code{integer} heightin pixel for container.  \code{height = 0} since the expectation is
+#'          for the container to be an invisible byproduct.
 #'
 #' @import htmlwidgets
 #'
